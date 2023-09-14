95e79bf34abd86fadde6590c2f021608904fbe2e
Harry737 <100664585+Harry737@users.noreply.github.com>
2023-09-14 22:54:25 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,6 +1,6 @@
 def call(Map params){
   withCredentials([usernameColonPassword(credentialsId: params.credential, variable: 'credential')]) {
-    sh "docker login -u $credential_USR -p $credential_PSW"
+    sh "docker login -u \$credential_USR -p \$credential_PSW"
     sh "docker push ${params.user}/${params.application}:${params.version}"
   }
 }
