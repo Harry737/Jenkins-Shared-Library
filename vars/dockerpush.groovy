cecaeb375b9d3e550e4470d4fc1829857c7c9111
Harry737 <100664585+Harry737@users.noreply.github.com>
2023-09-15 20:23:21 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -2,7 +2,7 @@ def call(Map params){
   withCredentials([usernamePassword(credentialsId: params.credential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
   // available as an env variable, but will be masked if you try to print it out any which way
   // note: single quotes prevent Groovy interpolation; expansion is by Bourne Shell, which is what you want
-    sh 'docker login -u $USERNAME -p $PASSWORD'
+    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
   }
   sh "docker push ${params.user}/${params.application}:${params.version}"
 }
